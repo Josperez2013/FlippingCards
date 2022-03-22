92eb185334badafcb4b2e57ef9d4ddd84dc3e403
Josperez2013 <55247335+Josperez2013@users.noreply.github.com>
2022-03-21 23:22:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: "#flashcards-app",
+    data: {
+      cards:[
+        {
+          question:"What year was IUPUI founded?",
+          answer:"1969",
+          category:"history",
+          flipped:false
+        },
+        {
+          question:"What is 2+2?",
+          answer:"Fish",
+          category:"math",
+          flipped:false
+        },{
+          question:"Is mayonnaise an instrument?",
+          answer:"No",
+          category:"science",
+          flipped:false
+        }
+      ]
+      
+    },
+      methods: {
+        flipCard: function(card){
+          card.flipped=!card.flipped;
+        },
+      }
+
+  })
+
+  let clickerEle=document.body.querySelector("#clicker");
+
+  
